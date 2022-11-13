5be6dbce9c45c15118ab3fb7a9d556dec2467416
Escutin Judithe (2019) ZHAC258 <judithe.escutin.2019@live.rhul.ac.uk>
2022-11-13 00:26:22 +0000

    Update PlayerMovement.cs for player movement (left and right) as well as making use of the Rigidbody 2D mechanism for the player to fall to the ground.

--- FinalYearProjectCode/PlayerMovement.cs
+++ FinalYearProjectCode/PlayerMovement.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    // public CharacterController controller;
 
     public float speed = 10f;
+
+    private Rigidbody2D rigidBody;
+
+    private void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 
     void Update() //Update is called once per frame
     {
         float xAxis = Input.GetAxis("Horizontal");
 
-        Vector2 movement = transform.right * xAxis;
+        // Vector2 movement = transform.right * xAxis;
 
-        controller.Move(movement * speed * Time.deltaTime);
+        // controller.Move(movement * speed * Time.deltaTime);
+
+        Vector2 movement = new Vector2(xAxis * speed, rigidBody.velocity.y);
+
+        rigidBody.velocity = movement;
     }
 }
