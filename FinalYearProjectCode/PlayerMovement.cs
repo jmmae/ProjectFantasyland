ff3a2b1abf6ff9b69fe5b342080af75cdb1e1c6f
Escutin Judithe (2019) ZHAC258 <judithe.escutin.2019@live.rhul.ac.uk>
2022-11-19 03:50:45 +0000

    Update PlayerMovement.cs to add a feature where sprite appearance changes to face towards the direction it's moving in and added a box collider (Raycast is used to detect hits in conjunction with a floor LayerMask) so the program knows when the player is touching the ground.

--- FinalYearProjectCode/PlayerMovement.cs
+++ FinalYearProjectCode/PlayerMovement.cs
@@ -2,31 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // public CharacterController controller;
+public class PlayerMovement : MonoBehaviour {
 
+    public float xAxis;
     public float speed = 10f;
     public float jumpSpeed = 5f;
 
     private Rigidbody2D rigidBody;
+    private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask floorLayer;
 
     private void Start() {
         rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    void Update() //Update is called once per frame
-    {
-        float xAxis = Input.GetAxis("Horizontal");
+    //Update is called once per frame
+    void Update() { 
+        xAxis = Input.GetAxis("Horizontal");
 
-        // Vector2 movement = transform.right * xAxis;
-
-        // controller.Move(movement * speed * Time.deltaTime);
-
+        // Setting up player movememnt so it can be manipulated
         Vector2 movement = new Vector2(xAxis * speed, rigidBody.velocity.y);
         rigidBody.velocity = movement;
 
-        if(Input.GetKey(KeyCode.Space))
+        // Allows player to jump:
+        if(isOnTheGround() && Input.GetKey(KeyCode.Space))
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+
+        // Allows player to change direction in appearance
+        if(xAxis < 0.01f)
+            transform.localScale = Vector3.one;
+        else if (xAxis > -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1); //Flip sprite in direction it's moving in
     }
+
+    // Checks if the player is on the ground using RaycastHit
+    private bool isOnTheGround() {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.2f, floorLayer);
+        return raycastHit.collider != null;
+    }
+
 }
