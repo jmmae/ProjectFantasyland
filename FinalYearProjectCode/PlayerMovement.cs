04be146f18dd9e179b9e6fe4a11b59491d61b5c4
Escutin Judithe (2019) ZHAC258 <judithe.escutin.2019@live.rhul.ac.uk>
2022-11-26 01:43:45 +0000

    Update PlayerMovement.cs to allow camera movement that will follow the player around the map. Limited sprite rotation so sprite doesn't rotate/flip. Changed speed and jumpSpeed to be faster. Change/flipped sprite appearance direction as previous sprite was looking in the wrong direction.

--- FinalYearProjectCode/PlayerMovement.cs
+++ FinalYearProjectCode/PlayerMovement.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float xAxis;
-    public float speed = 10f;
-    public float jumpSpeed = 5f;
+    public float speed = 30f;
+    public float jumpSpeed = 20f;
 
     private Rigidbody2D rigidBody;
     private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask floorLayer;
 
+    private GameObject cameraMovement;
+
     private void Start() {
         rigidBody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        cameraMovement = GameObject.Find("Main Camera");
     }
 
     //Update is called once per frame
@@ -30,10 +33,17 @@ public class PlayerMovement : MonoBehaviour {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
 
         // Allows player to change direction in appearance
-        if(xAxis < 0.01f)
+        if(xAxis > 0.01f)
             transform.localScale = Vector3.one;
-        else if (xAxis > -0.01f)
+        else if (xAxis < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1); //Flip sprite in direction it's moving in
+
+        cameraMovement.transform.position = new Vector3(rigidBody.position.x, rigidBody.position.y, -20); //Camera follows player
+    }
+    
+    void FixedUpdate() {
+        rigidBody.rotation = 0f; //Limits rotation of sprite
+        //Mathf.Lerp(rigidBody.rotation, 0, 0.5f);
     }
 
     // Checks if the player is on the ground using RaycastHit
