8b3337696afa52e97cdd47dc02275c06e740a529
Escutin Judithe (2019) ZHAC258 <judithe.escutin.2019@live.rhul.ac.uk>
2022-12-02 21:47:33 +0000

    Update Enemies.cs to account for damage inflicted on player, decreases lives counter when player hits enemy, makes use of debounce to prevent lives being lost too fast.

--- FinalYearProjectCode/Enemies.cs
+++ FinalYearProjectCode/Enemies.cs
@@ -7,15 +7,28 @@ public class Enemies : MonoBehaviour {
     public int direction = 1;
     public float distance = 5;
     public float speed = 5f;
+    public float debounce = 2f; //Number in seconds for hit debounce (prevents instant death)
+    private float debounceTimer = 0; //Internal number to increase and decrease (custom timer)
     private float startingPos;
 
     void Start() {
         startingPos = transform.position.x;
     }
 
+    // Negate time elapsed since last physics update
     void FixedUpdate() {
+        debounceTimer -= Time.deltaTime;
+
         transform.Translate(Vector2.right * Time.deltaTime * speed * direction); //Move the enemy to the right
         if (transform.position.x > startingPos + distance || transform.position.x < startingPos) 
             direction *= -1; //Change direction of sprite
+    }
+
+    //Checks if the player is colliding with an enemy and if the debounce timer isn't running
+    private void OnTriggerStay(Collider collide) { // OnTriggerStay runs as long as something is colliding
+        if (collide.transform.tag == "Player" && debounceTimer <= 0) {
+            debounceTimer = debounce;
+            PlayerMovement.decrementLives(); // Decreases lives by 1 when hit
+        }
     }
 }
