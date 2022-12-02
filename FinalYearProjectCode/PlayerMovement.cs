cb747b2368fff2bdf63540f11fd00d6aa0f42401
Escutin Judithe (2019) ZHAC258 <judithe.escutin.2019@live.rhul.ac.uk>
2022-12-02 21:44:15 +0000

    Update PlayerMovement.cs to add a livesCounter, set player spawn, change sprite flip mechanics to be more optimised, override player gravity, changed the raycast to support the new 3D rigidbody, created a decrementLives function. Converted the game from 2D to 3D.

--- FinalYearProjectCode/PlayerMovement.cs
+++ FinalYearProjectCode/PlayerMovement.cs
@@ -7,53 +7,72 @@ public class PlayerMovement : MonoBehaviour {
 
     public float xAxis;
     public float speed = 30f;
-    public float jumpSpeed = 20f;
+    public float jumpSpeed = 50f;
+    
     public static int coinCounter = 0;
+    public static int livesCounter = 3;
+    
     public TextMeshProUGUI coinOutput;
+    public TextMeshProUGUI livesOutput;
 
-    private Rigidbody2D rigidBody;
-    private BoxCollider2D boxCollider;
+    private Rigidbody rigidBody;
+    private BoxCollider boxCollider;
+    private SpriteRenderer spriteRenderer;
+    
     [SerializeField] private LayerMask floorLayer;
 
     private GameObject cameraMovement;
+    private GameObject playerSpawn;
 
     private void Start() {
-        rigidBody = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        rigidBody = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
         cameraMovement = GameObject.Find("Main Camera");
+        playerSpawn = GameObject.Find("PlayerSpawn");
+
+        rigidBody.transform.position = playerSpawn.transform.position; // Sets the player location to player spawn
     }
 
-    //Update is called once per frame
+    // Update is called once per frame
     void Update() { 
+        coinOutput.text = "Coins: " + coinCounter; // Renders current coins
+        livesOutput.text = "Lives: " + livesCounter; // Renders current lives
+    }
+    
+    void FixedUpdate() {
         xAxis = Input.GetAxis("Horizontal");
 
         // Setting up player movememnt so it can be manipulated
-        Vector2 movement = new Vector2(xAxis * speed, rigidBody.velocity.y);
+        Vector3 movement = new Vector3(xAxis * speed, rigidBody.velocity.y, 0);
         rigidBody.velocity = movement;
 
         // Allows player to jump:
         if(isOnTheGround() && Input.GetKey(KeyCode.Space))
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, jumpSpeed);
 
-        // Allows player to change direction in appearance
-        if(xAxis > 0f)
-            transform.localScale = Vector3.one;
-        else if (xAxis < -0f)
-            transform.localScale = new Vector3(-1, 1, 1); //Flip sprite in direction it's moving in
+        spriteRenderer.flipX = (xAxis < -0f); // Allows player to change direction in appearance
 
-        cameraMovement.transform.position = new Vector3(rigidBody.position.x, rigidBody.position.y, -20); //Camera follows player
-
-        coinOutput.text = "Coins: " + coinCounter; //Increases the Coin Counter in the UI
-    }
-    
-    void FixedUpdate() {
-        rigidBody.rotation = 0f; //Limits rotation of sprite
+        cameraMovement.transform.position = new Vector3(rigidBody.position.x, rigidBody.position.y, cameraMovement.transform.position.z) + new Vector3(0, 20, 0); //Camera follows player
+        
+        Physics.gravity = new Vector3(0, -100F, 0); // Overrides gravity of player
     }
 
-    // Checks if the player is on the ground using RaycastHit
+    // Checks if the player is on the ground using Raycast
     private bool isOnTheGround() {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.2f, floorLayer);
-        return raycastHit.collider != null;
+        RaycastHit raycastHit;
+        bool Raycast = Physics.BoxCast(boxCollider.bounds.center + new Vector3(0,3,0), boxCollider.bounds.size, Vector3.down, out raycastHit, transform.rotation, 2f, floorLayer);
+        return Raycast;
+    }
+
+    // Decreases lives counter
+    public static void decrementLives() {
+        // if(livesCounter > 0f) 
+        livesCounter--; 
+        //     rigidBody.transform.position = playerSpawn.transform.position; 
+        // else if(livesCounter <= 0f) 
+        //     print("no more lives :(");
+        //     rigidBody.transform.position = playerSpawn.transform.position;
     }
 
 }
