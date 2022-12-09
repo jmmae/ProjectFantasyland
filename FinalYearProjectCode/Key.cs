d7a1357332e214b6dbfac43931c1fedfe1a2c34f
Escutin Judithe (2019) ZHAC258 <judithe.escutin.2019@live.rhul.ac.uk>
2022-12-09 09:46:55 +0000

    Upload New File called Key.cs which checks if the player has collided with the key and if so, changes the boolean flag to true.

---
+++ FinalYearProjectCode/Key.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider collide) {
+        if (collide.transform.tag == "Player") {
+            PlayerMovement.keyObtained = true;
+            Destroy(gameObject);
+        }
+    }
+
+}
