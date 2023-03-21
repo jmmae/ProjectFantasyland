e0bb68379bf5fb644b6b7c8189577bce7a8c3e79
Escutin Judithe (2019) ZHAC258 <judithe.escutin.2019@live.rhul.ac.uk>
2023-03-21 01:39:25 +0000

    Update RegisterAccount.cs to remove redundant code.

--- FinalYearProjectCode/RegisterAccount.cs
+++ FinalYearProjectCode/RegisterAccount.cs
@@ -12,29 +12,21 @@ public class RegisterAccount : MonoBehaviour {
     public TextMeshProUGUI passwordInp;
     public Button registerButton;
     public Button loginButton;
-
     public GameObject levelMenu;
     public GameObject loginMenu;
-
     ArrayList accounts;
     
-    // Start is called before the first frame update
     void Start() {
         registerButton.onClick.AddListener(writeToFile);
-        loginButton.onClick.AddListener(login); //Go to level menu
 
-        string accountPath = Path.Combine(Application.dataPath, "/accounts.txt");
+        string accountPath = Application.dataPath + "/accounts.txt";
 
         if (File.Exists(accountPath)) {
             accounts = new ArrayList(File.ReadAllLines(accountPath));
         } else {
             File.WriteAllText(accountPath, string.Empty);
+            print("File not found.");
         }
-    }
-
-    void login() {
-        levelMenu.SetActive(true);
-        loginMenu.SetActive(false);
     }
 
     void writeToFile() {
@@ -42,15 +34,14 @@ public class RegisterAccount : MonoBehaviour {
 
         string accountPath = Application.dataPath +  "/accounts.txt";
         string[] readLines = File.ReadAllLines(accountPath);
-        print(accountPath);
 
-        //accounts = new ArrayList(File.ReadAllLines(Application.dataPath + "/accounts.txt"));
         foreach (string line in readLines) {
             if (line.Contains(usernameInp.text)) {
                 exists = true;
                 break;
             }
         }
+
         if (exists) {
             Debug.Log($"Username '{usernameInp.text}' already exists.");
         } else {
