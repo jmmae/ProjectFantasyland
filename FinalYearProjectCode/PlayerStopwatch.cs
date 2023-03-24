3fa2992cc6ec34f3634755709081c39ea23fbcee
Escutin Judithe (2019) ZHAC258 <judithe.escutin.2019@live.rhul.ac.uk>
2023-03-24 02:19:17 +0000

    Update PlayerStopwatch.cs to store player times into a file which is linked to the username that they logged in with.

--- FinalYearProjectCode/PlayerStopwatch.cs
+++ FinalYearProjectCode/PlayerStopwatch.cs
@@ -4,32 +4,54 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using TMPro;
+using System.IO;
 
 public class PlayerStopwatch : MonoBehaviour
 {
     
-    public static bool stopwatch = false;
+    public static int stopwatch = 1;
 
-    float currentTime;
+    float runTime;
     float finalTime;
 
-    public TextMeshProUGUI currentTimeText;
+    public TextMeshProUGUI runTimeText;
     public TextMeshProUGUI finalTimeText;
 
+    private string timesFilePath;
+
     void Start() {
-        currentTime = 0;
-        stopwatch = true;
+        runTime = 0;
+        stopwatch = 1;
+        
+        timesFilePath = Application.dataPath + "/playertimes.txt";
     }
 
     void Update() {
-        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        TimeSpan time = TimeSpan.FromSeconds(runTime);
     
-        if (stopwatch == true) {
-            currentTime = currentTime + Time.deltaTime;
-            currentTimeText.text = time.ToString(@"mm\:ss\:fff");
+        if (stopwatch == 1) {
+            runTime = runTime + Time.deltaTime;
+            runTimeText.text = time.ToString(@"mm\:ss\:fff");
+        } else if (stopwatch == 2) {
+            runTime = finalTime;
+            finalTimeText.text = runTimeText.text;
+            writeTimesToFile();
+            stopwatch = 3;
+        }
+        
+    }
+
+    void writeTimesToFile() {
+
+        if (File.Exists(timesFilePath)) {
+            using (var writeToFile = new StreamWriter(timesFilePath, append: true)) {
+                writeToFile.WriteLine($"{Login.usernameLogged}:{finalTimeText.text}");
+                print("Time Logged.");
+            }
         } else {
-            currentTime = finalTime;
-            finalTimeText.text = currentTimeText.text;
+            File.WriteAllText(timesFilePath, string.Empty);
+            print("File not found.");
+            writeTimesToFile();
         }
     }
 }
